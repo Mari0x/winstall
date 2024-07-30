c03a48a1a4208300ca773e494a3fb15a834600aa
Mariox <mari0x.baires@gmail.com>
2024-07-29 23:04:02 -0300

    Update install_software.ps1

--- install_software.ps1
+++ install_software.ps1
@@ -1,52 +1,54 @@
 function Install-Program {
     param(
         [Parameter(Mandatory=$true)]
-        [string]$programName,
-
-        [Parameter(Mandatory=$false)]
-        [string]$programVersion
+        [string]$programName
     )
 
-    # Lógica de instalación utilizando Winget o otros gestores de paquetes
     Write-Host "Instalando $programName..."
-    if ($programVersion) {
-        winget install "$programName" --version "$programVersion"
-    } else {
-        winget install "$programName"
-    }
+    # Lógica de instalación utilizando Winget o otros gestores de paquetes
+    winget install "$programName"
 
-    # Lista de programas disponibles
+    Write-Host "Instalación de $programName completada."
+}
+
+# Lista de programas disponibles
 $programList = @("WinRAR", "7-Zip", "Visual Studio Code", "Chrome", "Steam", "Discord", "VLC")
 
-# Inicializamos la variable para controlar el bucle
-$continue = $true
-
-while ($continue) {
-    Write-Host "Selecciona un programa a instalar"
+# Función para mostrar el menú de opciones de forma más clara
+function Show-Menu {
+    Write-Host "Selecciona un programa a instalar:"
     for ($i = 0; $i -lt $programList.Count; $i++) {
         Write-Host "$($i+1). $($programList[$i])"
     }
+}
 
-    $selectedOption = Read-Host "Ingrese el número de opción:"
+# Bucle principal
+do {
+    Show-Menu
 
-    # Validar la opción seleccionada
+    $selectedOption = Read-Host "Ingrese el número de opción (o 'q' para salir)"
+
+    if ($selectedOption -eq 'q') {
+        break
+    }
+
     if ($selectedOption -gt $programList.Count -or $selectedOption -lt 1) {
-        Write-Host "Opción inválida."
+        Write-Host "Opción inválida. Intenta nuevamente."
         continue
     }
 
-    # Obtener el programa seleccionado
     $program = $programList[$selectedOption - 1]
 
-    # Preguntar si desea instalar el programa seleccionado
-    $confirm = Read-Host "¿Desea instalar $program ? (s/n)" -eq "s"
+    # Confirmar la instalación
+    $confirm = Read-Host "¿Estás seguro de que deseas instalar $program? (s/n)" -eq "s"
     if ($confirm) {
         Install-Program -programName $program
-        Write-Host "Instalación completa."
+    } else {
+        Write-Host "Instalación cancelada."
     }
 
-    # Preguntar si desea instalar otro programa y actualizar la variable $continue
-    $continue = Read-Host "¿Desea instalar otro programa? (s/n)" -eq "s"
-}
+    # Preguntar si desea instalar otro programa
+    $continue = Read-Host "¿Deseas instalar otro programa? (s/n)" -eq "s"
+} while ($continue)
 
 Write-Host "¡Hasta luego!"
